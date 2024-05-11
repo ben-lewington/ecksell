e5dd2ea27b559a78f7f21d6f15c905f52dd0e254
Ben Lewington <ben.lewington1991@gmail.com>
2024-05-11 01:55:29 +0100

    bs

--- src/array.h
+++ src/array.h
@@ -6,34 +6,27 @@
 #ifndef EVERY_H
 #define EVERY_H
 
-#include <assert.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define Array(TypeName, Item) typedef \
+struct {                              \
+    Item *items;                      \
+    size_t count;                     \
+    size_t cap;                       \
+} TypeName
 
-#define every_dyn_arr(ident, T) typedef \
-struct {                                \
-    T *items;                           \
-    size_t count;                       \
-    size_t cap;                         \
-} ident
+#define ARRAY_DEFAULT_CAPACITY 256
 
-#define EVERY_DYN_ARR_INIT_CAP 256
-
-#define every_dyn_arr_append(arr, item) do {                                        \
+#define array_push(arr, item) do {                                        \
     if ((arr)->count >= (arr)->cap) {                                               \
-        (arr)->cap = (arr)->cap == 0 ? EVERY_DYN_ARR_INIT_CAP : (arr)->cap * 2;     \
+        (arr)->cap = (arr)->cap == 0 ? ARRAY_DEFAULT_CAPACITY : (arr)->cap * 2;     \
         (arr)->items = realloc((arr)->items, (arr)->cap * sizeof(*((arr)->items))); \
     }                                                                               \
     (arr)->items[(arr)->count++] = (item);                                          \
 } while(0)
 
-#define every_dyn_arr_extend(arr, its, its_count) do {                               \
+#define array_extend(arr, its, its_count) do {                               \
     if ((arr)->count + its_count >= (arr)->cap) {                                    \
         if ((arr)->cap == 0) {                                                       \
-            (arr)->cap = EVERY_DYN_ARR_INIT_CAP;                                     \
+            (arr)->cap = ARRAY_DEFAULT_CAPACITY;                                     \
         }                                                                            \
         while ((arr)->count + its_count > (arr)->cap) {                              \
             (arr)->cap *= 2;                                                         \
@@ -44,9 +37,6 @@ struct {                                \
     (arr)->count += its_count;                                                       \
 } while(0)
 
-#define every_dyn_arr_free(arr) free((arr)->items)
+#define array_free(arr) free((arr)->items)
 
-#ifdef EVERY_IMPL
-
-#endif /* ifndef EVERY_IMPL */
 #endif /* ifndef EVERY_H */
