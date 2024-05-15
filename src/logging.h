6b19e40544771d3e815f737106e995a9ec7ab897
Ben Lewington <ben.lewington1991@gmail.com>
2024-05-15 23:11:11 +0100

    updating the logging

--- src/logging.h
+++ src/logging.h
@@ -2,37 +2,37 @@
 #define LOG_H
 
 #include <assert.h>
+#include <stdarg.h>
 #include <stdio.h>
 
 #ifndef __log_impl
-#define __log_impl(fmt, ...) fprintf(stderr, fmt "\n", __VA_ARGS__)
+#define __log_impl(fmt, ...) fprintf(stderr, fmt "\n"  __VA_OPT__(,) __VA_ARGS__)
 #endif // __log_impl
 
-#define LOG_DEBUG 0
-#define LOG_INFO  1
-#define LOG_WARN  2
-#define LOG_ERROR 3
+#define LOG_ERROR 0
+#define LOG_DEBUG 1
+#define LOG_INFO  2
+#define LOG_WARN  3
 
 typedef enum {
+    error = LOG_ERROR,
     debug = LOG_DEBUG,
     info  = LOG_INFO,
     warn  = LOG_WARN,
-    error = LOG_ERROR,
 } LogLevel;
 
 /// log a value at any log level at runtime
 void log_fmt(LogLevel lvl, char *fmt, ...);
 
-#define __ldebug(fmt, ...) __log_impl("[DEBUG]: %s:%d: " fmt "\n", __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
-#define __linfo(fmt, ...)  __log_impl("[INFO] : %s:%d: " fmt "\n", __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
-#define __lwarn(fmt, ...)  __log_impl("[WARN] : %s:%d: " fmt "\n", __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
-#define __lerror(fmt, ...) __log_impl("[ERROR]: %s:%d: " fmt "\n", __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+#define log_error(fmt, ...) __log_impl("[ERROR] %s:%d: " fmt, __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+#define __ldebug(fmt, ...)  __log_impl("[DEBUG] %s:%d: " fmt, __FILE__, __LINE__ __VA_OPT__(,) __VA_ARGS__)
+#define __linfo(fmt, ...)   __log_impl("[INFO ]  " fmt                           __VA_OPT__(,) __VA_ARGS__)
+#define __lwarn(fmt, ...)   __log_impl("[WARN ]  " fmt                           __VA_OPT__(,) __VA_ARGS__)
 
 #ifndef LOG_LEVEL
-    #define log_debug(fmt, ...) __ldebug(fmt, __VA_ARGS__)
-    #define log_info(fmt, ...)  __linfo(fmt, __VA_ARGS__)
-    #define log_warn(fmt, ...)  __lwarn(fmt, __VA_ARGS__)
-    #define log_error(fmt, ...) __lerror(fmt, __VA_ARGS__)
+    #define log_debug(fmt, ...) __ldebug(fmt __VA_OPT__(,) __VA_ARGS__)
+    #define log_info(fmt, ...)  __linfo(fmt __VA_OPT__(,) __VA_ARGS__)
+    #define log_warn(fmt, ...)  __lwarn(fmt  __VA_OPT__(,) __VA_ARGS__)
 #else
     #if ((LOG_LEVEL) <= LOG_DEBUG)
         #define log_debug(fmt, ...) __ldebug(fmt, __VA_ARGS__)
@@ -49,8 +49,7 @@ void log_fmt(LogLevel lvl, char *fmt, ...);
     #else
         #define every_warn(fmt, ...)
     #endif
-    #define log_error(fmt, ...)     __lerror(fmt, __VA_ARGS__)
-#endif
+#endif // !LOG_LEVEL
 #endif // LOG_H
 
 #ifdef LOGGING_IMPL
